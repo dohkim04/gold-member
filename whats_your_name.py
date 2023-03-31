c93e103bdf350534255f8f9e5936555eb3ad67f4
Zaire Ali <zaireali649@gmail.com>
2023-03-31 00:01:29 +0000

    whats_your_name

--- whats_your_name.py
+++ whats_your_name.py
@@ -9,9 +9,11 @@
 
 def print_full_name(first, last):
     # Write your code here
+    print(__name__)
     print("Hello " + first + " " + last + "! You just delved into python.")
 
 if __name__ == '__main__':
     first_name = input("Input first name: ")
     last_name = input("Input last name: ")
     print_full_name(first_name, last_name)
+    print_full_name("Ross", "Taylor")
